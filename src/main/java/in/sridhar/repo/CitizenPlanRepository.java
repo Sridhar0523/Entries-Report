2e204d0d30cd3eb210b0537786c7739e7b609cba
Sridhar0523 <log4sridhar@gmail.com>
2022-12-22 21:15:54 +0530

    updated Code

--- src/main/java/in/sridhar/repo/CitizenPlanRepository.java
+++ src/main/java/in/sridhar/repo/CitizenPlanRepository.java
@@ -1,11 +1,20 @@
 package in.sridhar.repo;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import in.sridhar.binding.CitizenPlan;
 
 public interface CitizenPlanRepository extends JpaRepository<CitizenPlan, Serializable> {
+	
+	@Query("select distinct(planName) from CitizenPlan")
+	public List<String> getPlanNames();
+	
+	@Query("select distinct(planStatus) from CitizenPlan")
+	public List<String> getPlanStatus();
+
 
 }
--- src/main/java/in/sridhar/service/ReportServiceImpl.java
+++ src/main/java/in/sridhar/service/ReportServiceImpl.java
@@ -5,45 +5,66 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import in.sridhar.binding.CitizenPlan;
 import in.sridhar.binding.SearchRequest;
+import in.sridhar.repo.CitizenPlanRepository;
 
 @Service
 public class ReportServiceImpl implements ReportService {
-	
+
 	@Autowired
-	private ReportService reportService;
+	private CitizenPlanRepository citizenPlanRepository;
 
 	@Override
 	public List<String> getPlanNames() {
-		List<String> planNames = reportService.getPlanNames();
+		List<String> planNames = citizenPlanRepository.getPlanNames();
 		return planNames;
 	}
 
 	@Override
 	public List<String> getPlanStatus() {
-		List<String> planStatus = reportService.getPlanStatus();
+		List<String> planStatus = citizenPlanRepository.getPlanStatus();
 		return planStatus;
 	}
 
 	@Override
 	public List<CitizenPlan> getCitizenPlans(SearchRequest request) {
-		List<CitizenPlan> citizenPlans = reportService.getCitizenPlans(request);
-		return citizenPlans;
+
+		CitizenPlan entity = new CitizenPlan();
+
+		if (request.getPlanName() != null && !request.getPlanName().equals("")) {
+			entity.setPlanName(request.getPlanName());
+		}
+
+		if (request.getPlanStatus() != null && !request.getPlanStatus().equals("")) {
+			entity.setPlanStatus(request.getPlanStatus());
+		}
+
+		if (request.getGender() != null && !request.getGender().equals("")) {
+			entity.setGender(request.getGender());
+		}
+
+		Example<CitizenPlan> example = Example.of(entity);
+
+		List<CitizenPlan> records = citizenPlanRepository.findAll(example);
+
+		return records;
+
 	}
 
 	@Override
 	public void exportExcel(HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void exportPdf(HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
